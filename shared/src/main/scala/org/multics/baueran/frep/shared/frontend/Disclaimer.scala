eb9384f352282c558423b2b2dd30d808de58fd30
Andreas Bauer <baueran@gmail.com>
2019-09-08 19:59:21 +0200

    Beautify

--- shared/src/main/scala/org/multics/baueran/frep/shared/frontend/Disclaimer.scala
+++ shared/src/main/scala/org/multics/baueran/frep/shared/frontend/Disclaimer.scala
@@ -49,7 +49,7 @@ object Disclaimer {
         " | ",
         a(cls:="underline", style:="color:white;", href:="#", onclick:= { () => loadAndScroll("contact.html") }, "Contact"),
         " | ",
-        a(cls:="underline", style:="color:white;", href:="#", onclick:= { () => loadAndScroll("cookies.html") }, "Privacy policy"),
+        a(cls:="underline", style:="color:white;", href:="#", onclick:= { () => loadAndScroll("cookies.html") }, "Privacy Policy"),
       )
     )
   }
