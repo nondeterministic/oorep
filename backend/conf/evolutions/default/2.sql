3ee5d15b8b29f15aa16e46326e5546b9500b9b53
Andreas Bauer <baueran@gmail.com>
2019-11-10 11:31:41 +0100

    Extend member's DB table with lastseen and nooflogins

--- backend/conf/evolutions/default/2.sql
+++ backend/conf/evolutions/default/2.sql
@@ -1,6 +1,9 @@
 # --- !Ups
 
-INSERT INTO MEMBER(member_id, member_name, hash, realname, email, country, title, profession) VALUES (1, 'baueran', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb', 'Andreas Bauer', 'a@pspace.org', 'Germanistan', 'Mr.', 'Professional');
-INSERT INTO MEMBER(member_id, member_name, hash, realname, email, country, title, profession) VALUES (2, 'ssj', ' ', 'Social Justice Warrior', 'ssj@ksks.com', 'Canada', 'Cis', 'Student');
+ALTER TABLE MEMBER ADD LASTSEEN VARCHAR(255);
+ALTER TABLE MEMBER ADD NUMBEROFLOGINS INT DEFAULT 0;
 
-# -- !Downs
+# --- !Downs
+
+ALTER TABLE MEMBER DROP LASTSEEN;
+ALTER TABLE MEMBER DROP NUMBEROFLOGINS;
