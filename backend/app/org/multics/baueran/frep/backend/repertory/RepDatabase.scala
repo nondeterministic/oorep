bc274117f670c934b0830687f19c0c0caa80d812
Andreas Bauer <a@pspace.org>
2020-10-04 10:59:15 +0200

    Introduce OOREP_IMPORT_REPERTORIES, more accurate repertory descriptions in faq and impressum

--- backend/app/org/multics/baueran/frep/backend/repertory/RepDatabase.scala
+++ backend/app/org/multics/baueran/frep/backend/repertory/RepDatabase.scala
@@ -67,73 +67,80 @@ object RepDatabase {
       repInfos.toList
     }
 
-    availableRepertoriesOnDisk().foreach(repInfo => {
-      val repertory = Repertory.loadFrom(localRepPath(), repInfo.abbrev)
+    sys.env.get("OOREP_IMPORT_REPERTORIES") match {
+      case Some(answer) if (answer.toUpperCase == "YES" || answer.toUpperCase == "TRUE" || answer.toUpperCase == "1") => {
+        Logger.debug("Skipping repertory import as env-var OOREP_IMPORT_REPERTORIES not set to YES.")
+        availableRepertoriesOnDisk().foreach(repInfo => {
+          val repertory = Repertory.loadFrom(localRepPath(), repInfo.abbrev)
 
-      if (dao.getInfo(repInfo.abbrev).size == 0) {
-        Logger.debug("Inserting info into DB for " + repInfo.abbrev + ".")
-        dao.insert(repInfo)
-      } else {
-        Logger.debug("NOT inserting info into DB for " + repInfo.abbrev + ".")
+          if (dao.getInfo(repInfo.abbrev).size == 0) {
+            Logger.debug("Inserting info into DB for " + repInfo.abbrev + ".")
+            dao.insert(repInfo)
+          } else {
+            Logger.debug("NOT inserting info into DB for " + repInfo.abbrev + ".")
+          }
+
+          if (repertory.chapters.size > 0 && dao.getChapter(repertory.chapters.last.id).size == 0) {
+            Logger.debug("Inserting chapters into DB for " + repertory.info.abbrev + ".")
+            repertory.chapters.foreach(dao.insert(_))
+          } else {
+            Logger.debug("NOT inserting chapters into DB for " + repertory.info.abbrev + ".")
+          }
+
+          if (repertory.rubricRemedies.size > 0 && dao.getRubricRemedy(repertory.rubricRemedies.last).size == 0) {
+            Logger.debug("Inserting rubric remedies into DB for " + repertory.info.abbrev + ".")
+            repertory.rubricRemedies.foreach(dao.insert(_))
+          } else {
+            Logger.debug("NOT inserting rubric remedies into DB for " + repertory.info.abbrev + ".")
+          }
+
+          if (repertory.remedies.size > 0 && dao.getRemedy(repertory.remedies.last.abbrev, repertory.remedies.last.id).size == 0) {
+            Logger.debug("Inserting remedies into DB for " + repertory.info.abbrev + ".")
+            repertory.remedies.foreach(dao.insert(_))
+          } else {
+            Logger.debug("NOT inserting remedies into DB for " + repertory.info.abbrev + ".")
+          }
+
+          if (repertory.chapterRemedies.size > 0 && dao.getChapterRemedy(repertory.chapterRemedies.last).size == 0) {
+            Logger.debug("Inserting chapter remedies into DB for " + repertory.info.abbrev + ".")
+            repertory.chapterRemedies.foreach(dao.insert(_))
+          } else {
+            Logger.debug("NOT inserting chapter remedies into DB for " + repertory.info.abbrev + ".")
+          }
+
+          if (repertory.rubrics.size > 0 && dao.getRubric(repertory.rubrics.last.abbrev, repertory.rubrics.last.id).size == 0) {
+            Logger.debug("Inserting rubrics into DB for " + repertory.info.abbrev + ".")
+            repertory.rubrics.foreach(dao.insert(_))
+          } else {
+            Logger.debug("NOT inserting rubrics into DB for " + repertory.info.abbrev + ".")
+          }
+
+        })
       }
-
-      if (repertory.chapters.size > 0 && dao.getChapter(repertory.chapters.last.id).size == 0) {
-        Logger.debug("Inserting chapters into DB for " + repertory.info.abbrev + ".")
-        repertory.chapters.foreach(dao.insert(_))
-      } else {
-        Logger.debug("NOT inserting chapters into DB for " + repertory.info.abbrev + ".")
-      }
-
-      if (repertory.rubricRemedies.size > 0 && dao.getRubricRemedy(repertory.rubricRemedies.last).size == 0) {
-        Logger.debug("Inserting rubric remedies into DB for " + repertory.info.abbrev + ".")
-        repertory.rubricRemedies.foreach(dao.insert(_))
-      } else {
-        Logger.debug("NOT inserting rubric remedies into DB for " + repertory.info.abbrev + ".")
-      }
-
-      if (repertory.remedies.size > 0 && dao.getRemedy(repertory.remedies.last.abbrev, repertory.remedies.last.id).size == 0) {
-        Logger.debug("Inserting remedies into DB for " + repertory.info.abbrev + ".")
-        repertory.remedies.foreach(dao.insert(_))
-      } else {
-        Logger.debug("NOT inserting remedies into DB for " + repertory.info.abbrev + ".")
-      }
-
-      if (repertory.chapterRemedies.size > 0 && dao.getChapterRemedy(repertory.chapterRemedies.last).size == 0) {
-        Logger.debug("Inserting chapter remedies into DB for " + repertory.info.abbrev + ".")
-        repertory.chapterRemedies.foreach(dao.insert(_))
-      } else {
-        Logger.debug("NOT inserting chapter remedies into DB for " + repertory.info.abbrev + ".")
-      }
-
-      if (repertory.rubrics.size > 0 && dao.getRubric(repertory.rubrics.last.abbrev, repertory.rubrics.last.id).size == 0) {
-        Logger.debug("Inserting rubrics into DB for " + repertory.info.abbrev + ".")
-        repertory.rubrics.foreach(dao.insert(_))
-      } else {
-        Logger.debug("NOT inserting rubrics into DB for " + repertory.info.abbrev + ".")
-      }
-
-    })
+      case None =>
+        Logger.debug("Skipping repertory import as env-var OOREP_IMPORT_REPERTORIES not set to YES.")
+    }
 
     Logger.info("DB setup complete.")
   }
 
-  @deprecated("Only used for testing.","24-02-2019")
-  def repertory(abbrev: String): Option[Repertory] = {
-    def loadAndPutRepertory(abbrev: String) = {
-      if (availableRepertoriesAbbrevs.contains(abbrev)) {
-        repertories.put(abbrev, Repertory.loadFrom(localRepPath(), abbrev))
-        Logger.debug(s"INFO: RepDatabase: Server: repertory $abbrev loaded.")
-      }
-      else
-        Logger.debug(s"ERROR: RepDatabase: Failed to load repertory ${abbrev} as it is not available.")
-    }
-
-    if (availableRepertoriesAbbrevs.contains(abbrev) && !repertories.contains(abbrev))
-      loadAndPutRepertory(abbrev)
-    else if (!availableRepertoriesAbbrevs.contains(abbrev))
-      None
-
-    repertories.get(abbrev)
-  }
+//  @deprecated("Only used for testing.","24-02-2019")
+//  def repertory(abbrev: String): Option[Repertory] = {
+//    def loadAndPutRepertory(abbrev: String) = {
+//      if (availableRepertoriesAbbrevs.contains(abbrev)) {
+//        repertories.put(abbrev, Repertory.loadFrom(localRepPath(), abbrev))
+//        Logger.debug(s"INFO: RepDatabase: Server: repertory $abbrev loaded.")
+//      }
+//      else
+//        Logger.debug(s"ERROR: RepDatabase: Failed to load repertory ${abbrev} as it is not available.")
+//    }
+//
+//    if (availableRepertoriesAbbrevs.contains(abbrev) && !repertories.contains(abbrev))
+//      loadAndPutRepertory(abbrev)
+//    else if (!availableRepertoriesAbbrevs.contains(abbrev))
+//      None
+//
+//    repertories.get(abbrev)
+//  }
 
 }
