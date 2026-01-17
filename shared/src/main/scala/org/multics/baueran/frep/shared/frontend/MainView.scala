efad3ce49ebd5d965767e4184c7b1d2ad61f5845
nondeterministic <a@pspace.org>
2026-01-17 17:41:45 +0100

    snapshot

--- shared/src/main/scala/org/multics/baueran/frep/shared/frontend/MainView.scala
+++ shared/src/main/scala/org/multics/baueran/frep/shared/frontend/MainView.scala
@@ -86,7 +86,7 @@ object MainView {
             case (aboutDiv: dom.Element, featuresDiv: dom.Element) =>
               dom.document.body.removeChild(aboutDiv)
               dom.document.body.removeChild(featuresDiv)
-            case null => ;
+            case _ => ;
           }
         }
       }
