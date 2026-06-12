730ae7b70c1ec4e3a2bd6ac40aa61e6ba2c1de5d
nondeterministic <a@pspace.org>
2026-06-12 18:07:06 +0200

    Oops... forgot Settings Dialog

---
+++ shared/src/main/scala/org/multics/baueran/frep/shared/sec_frontend/SettingsModal.scala
@@ -0,0 +1,144 @@
+package org.multics.baueran.frep.shared.sec_frontend
+
+import org.multics.baueran.frep.shared.Defs.{CookieFields, HeaderFields}
+import org.multics.baueran.frep.shared.{FIle, HttpRequest2}
+import org.multics.baueran.frep.shared.frontend.{CaseSection, Notify, OorepHtmlButton, OorepHtmlElement, OorepHtmlInput, OorepHtmlTextArea, getCookieData}
+import scalatags.JsDom.all.*
+import org.scalajs.dom
+import org.scalajs.dom.{Event, html}
+
+import scala.scalajs.js
+import io.circe.syntax._
+import org.multics.baueran.frep.shared.TopLevelUtilCode.getDocumentCsrfCookie
+
+object SettingsModal extends OorepHtmlElement {
+  def getId() = "SettingsModal_EEEdsfakjDD345nmbertertertv8f89HG34jkdf8934RSCjfr8943jkarsklfg"
+
+  object CloseButton extends OorepHtmlButton {
+    def getId() = "SettingsModal_CloseButton_sdcfdfdferertfdsf24532"
+
+    def apply() = {
+      button(id := getId(), `type` := "button", cls := "close", data.dismiss := "modal", "\u00d7")
+    }
+  }
+
+  object Form extends OorepHtmlElement {
+    def getId() = "SettingsModal_Form_7659587ertretertfdvdf032424jhbjd32sfGGHjhHKJEEWYYCVVKGHFJHD56476476"
+
+    object SubmitButton extends OorepHtmlButton {
+      def getId() = "SettingsModal_Form_SubmitButton_dsfretertretsdfkjhsdklj324234nkbsdfkljh34rt34kl"
+
+      def onSubmit = (event: Event) => {
+        event.preventDefault() // Without this, the submit button also closes on wrong input and reloads the application
+
+        val memberId = getCookieData(dom.document.cookie, CookieFields.id.toString) match {
+          case Some(id) => id.toInt
+          case None => -1 // TODO: Force user to relogin; the identification cookie has disappeared!!!!!!!!!!
+        }
+
+        if (HeaderInput.getText().trim.length == 0 || OpenFileModal.headers().count(_ == HeaderInput.getText().trim) != 0) {
+          if (Notify.noAlertsVisible())
+            new Notify("tempFeedbackAlert", "Saving file failed. Make sure file ID is unique and not empty!")
+        }
+        else {
+          val currFIle = Some(FIle(None, HeaderInput.getText().trim, memberId, (new js.Date()).toISOString(), (new js.Date()).toISOString(), DescriptionTextArea.getText(), List.empty))
+
+          HttpRequest2("sec/save_file")
+            .withHeaders((HeaderFields.csrfToken.toString(), getDocumentCsrfCookie().getOrElse("")))
+            .onSuccess((_) => {
+              CaseSection.updateCaseViewAndDataStructures()
+              HeaderInput.setText("")
+              DescriptionTextArea.setText("")
+              SettingsModal.CloseButton.click()
+            })
+            .onFailure((_) => {
+              if (Notify.noAlertsVisible())
+                new Notify("tempFeedbackAlert", "Saving file failed. Make sure file ID is unique!")
+            })
+            .post(currFIle.get.asJson.toString())
+        }
+      }
+
+      def apply() = {
+        button(cls := "btn btn-primary mb-2", style := "margin-left:8px;", `type` := "submit", id := getId(), disabled := true,
+          onclick := onSubmit,
+          "Submit")
+      }
+    }
+
+    object CancelButton extends OorepHtmlButton {
+      def getId() = "SettingsModal_Form_CancelButton_dsfHJGJHGksdfkjertreterthsdklj324234nkbsdfkljh34rt34kl"
+
+      def apply() = {
+        button(cls := "btn mb-2 btn-secondary", data.dismiss := "modal",
+          "Cancel",
+          onclick := { (event: Event) =>
+            event.preventDefault()
+            HeaderInput.setText("")
+            DescriptionTextArea.setText("")
+          })
+      }
+    }
+
+    object HeaderInput extends OorepHtmlInput {
+      def getId() = "SettingsModal_Form_HeaderInput_nbmnbNMBerNMgerertertBHJGJHGUIF7657687ghzxcjksd23234IFY"
+
+      def apply() = {
+        input(cls := "form-control",
+          id := getId(),
+          placeholder := "A simple, unique file identifier",
+          required,
+          oninput := { (event: dom.KeyboardEvent) =>
+            if (getText().trim.length > 0)
+              SubmitButton.enable()
+            else
+              SubmitButton.disable()
+          }
+        )
+      }
+    }
+    object DescriptionTextArea extends OorepHtmlTextArea {
+      def getId() = "SettingsModal_Form_DescriptionTextArea_HJJHGJKGertretretJGKJG4534534ertretert532535345345423"
+
+      def apply() =
+        textarea(cls := "form-control", id := getId(), rows := "3", placeholder := "A more verbose description of the file")
+    }
+
+    def apply() = {
+      form(id := getId(),
+        div(cls := "form-group",
+          label(`for` := HeaderInput.getId(), "ID"),
+          HeaderInput()
+        ),
+        div(cls := "form-group",
+          label(`for` := DescriptionTextArea.getId(), "Description"),
+          DescriptionTextArea()
+        ),
+        div(cls := "d-flex flex-row-reverse",
+          SubmitButton(),
+          CancelButton()
+        )
+      )
+    }
+  }
+
+  def apply() = {
+    println("SettingsModal!!!")
+    div(cls:="modal fade", tabindex:="-1", role:="dialog", id:=getId(),
+      onshow := { (event: Event) => Form.SubmitButton.disable() },
+      div(cls:="modal-dialog modal-dialog-centered", role:="document", style:="min-width: 80%;",
+        div(cls:="modal-content",
+          div(cls:="modal-header",
+            h5(cls:="modal-title", "Create a new file"),
+            CloseButton(),
+          ),
+          div(cls:="modal-body",
+            div(cls:="table-responsive",
+              Form()
+            )
+          )
+        )
+      )
+    )
+  }
+}
